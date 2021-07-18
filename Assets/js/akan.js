99e9563cab98f6e23a5c83197d98107f199bf4e6
Marvin Maina <machariamarvin625@gmail.com>
2021-07-18 14:13:18 +0300

    Make submit button transparent when inputs are empty

--- Assets/js/akan.js
+++ Assets/js/akan.js
@@ -1,3 +1,5 @@
+// validate form
+
 function validate(){
     var a = document.getElementById("username").value;
     var b = document.getElementById("birthdate").value;
@@ -5,8 +7,48 @@ function validate(){
     var d = document.getElementById("user-ex");
     var e = document.getElementById("birth-ex");
     var f = document.getElementById("gender-ex");
+    var g = document.getElementById("f-submit");
 
     if (a === "" || a === null){
         d.style.display = "block";
     }
 }
+
+ // Make submit button transparent if inputs are empty
+
+function check(){
+    var a = document.getElementById("username").value;
+    var b = document.getElementById("birthdate").value;
+    var c = document.getElementById("gender").value;
+    var d = document.getElementById("user-ex");
+    var e = document.getElementById("birth-ex");
+    var f = document.getElementById("gender-ex");
+    var g = document.getElementById("f-submit");
+
+    if (a === "" || a === null){
+        g.style.cursor = "not-allowed";
+        g.style.background = "transparent";
+        return false;
+    }
+
+    else if(b === "" || b === null){
+        g.style.cursor = "not-allowed";
+        g.style.background = "transparent";
+        return false;
+    }
+
+    else if(c === ""){
+        g.style.cursor = "not-allowed";
+        g.style.background = "transparent";
+        return false;
+    }
+
+    else{
+        g.style.cursor = "pointer";
+        background = "gold";
+        return true;
+    }
+}
+
+
+
