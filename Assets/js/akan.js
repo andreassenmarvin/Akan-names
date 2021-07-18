b4eb6856f6764285605e5fcd1092e0c8cfab3caf
Marvin Maina <machariamarvin625@gmail.com>
2021-07-18 15:50:49 +0300

    Add validation to form

--- Assets/js/akan.js
+++ Assets/js/akan.js
@@ -1,30 +1,37 @@
+var a = document.getElementById("username").value;
+var b = document.getElementById("birthdate").value;
+var c = document.getElementById("gender").value;
+var d = document.getElementById("user-ex");
+var e = document.getElementById("birth-ex");
+var f = document.getElementById("gender-ex");
+var g = document.getElementById("f-submit");
+
+
+
 // validate form
 
 function validate(){
-    var a = document.getElementById("username").value;
-    var b = document.getElementById("birthdate").value;
-    var c = document.getElementById("gender").value;
-    var d = document.getElementById("user-ex");
-    var e = document.getElementById("birth-ex");
-    var f = document.getElementById("gender-ex");
-    var g = document.getElementById("f-submit");
 
-    if (a === "" || a === null){
+    if (a === null || a === ""){
         d.style.display = "block";
+        return false;
+    }
+
+    else if (b === null || b === ""){
+        e.style.display = "block";
+        return false
+    }
+
+    else if (c === null || c === ""){
+        f.style.display = "block";
+        return false
     }
 }
 
  // Make submit button transparent if inputs are empty
 
 function check(){
-    var a = document.getElementById("username").value;
-    var b = document.getElementById("birthdate").value;
-    var c = document.getElementById("gender").value;
-    var d = document.getElementById("user-ex");
-    var e = document.getElementById("birth-ex");
-    var f = document.getElementById("gender-ex");
-    var g = document.getElementById("f-submit");
-
+   
     if (a === "" || a === null){
         g.style.cursor = "not-allowed";
         g.style.background = "transparent";
